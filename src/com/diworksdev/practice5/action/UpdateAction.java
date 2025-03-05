a0bf5ccc23e168b70aed20d486bcdea236739354
sakitamako <sa.919.1025.ki@gmail.com>
2025-03-05 23:15:23 +0900

    クラスの内容ごちゃごちゃになってしまった、リセットしてやり直す

--- src/com/diworksdev/practice5/action/UpdateAction.java
+++ src/com/diworksdev/practice5/action/UpdateAction.java
@@ -1,5 +1,7 @@
 package com.diworksdev.practice5.action;
 
+import java.sql.SQLException;
+
 import com.diworksdev.practice5.dao.RegistCompleteDAO;
 import com.diworksdev.practice5.dto.UserDTO;
 import com.opensymphony.xwork2.ActionSupport;
@@ -15,6 +17,7 @@ public class UpdateAction extends ActionSupport {
 	private String userPassword;
 	private int userGender;
 	private String userPostalCode;
+//	private String userPostalCodeCheck;
 	private String userPrefecture;
 	private String userAddress1;
 	private String userAddress2;
@@ -22,125 +25,136 @@ public class UpdateAction extends ActionSupport {
 
 	@Override
 	public String execute() {
-		if (userId <= 0) {
-			return SUCCESS; // 新規登録の場合はそのまま画面遷移
-		}
 
-		RegistCompleteDAO dao = new RegistCompleteDAO();
-		try {
-			UserDTO user = dao.getUserById(userId); // DBからユーザー情報を取得
+		// userId が 0 の場合は新規登録、それ以外なら更新処理（データ取得）
+        if (userId > 0) {
+            // DAO を使って userId に該当するユーザー情報を取得
+            RegistCompleteDAO dao = new RegistCompleteDAO();
+            UserDTO user;
 
-			if (user == null) {
-				addActionError("該当するユーザーが見つかりません。");
-				return ERROR;
-			}
+            try {
 
-			// 取得した情報をセット
-			this.userFamilyName = user.getUserFamilyName();
-			this.userLastName = user.getUserLastName();
-			this.userFamilyNameKana = user.getUserFamilyNameKana();
-			this.userLastNameKana = user.getUserLastNameKana();
-			this.userMail = user.getUserMail();
-			this.userPassword = user.getUserPassword();
-			this.userGender = user.getUserGender();
-			this.userPostalCode = user.getUserPostalCode();
-			this.userPrefecture = user.getUserPrefecture();
-			this.userAddress1 = user.getUserAddress1();
-			this.userAddress2 = user.getUserAddress2();
-			this.userAuthority = user.getUserAuthority();
+                user = dao.getUserById(userId);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			addActionError("ユーザー情報の取得中にエラーが発生しました。");
-			return ERROR;
-		}
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+
+                return ERROR;
+
+            }
+
+            if (user != null) {
+                // 取得した情報をセット
+                this.userFamilyName = user.getUserFamilyName();
+                this.userLastName = user.getUserLastName();
+                this.userFamilyNameKana = user.getUserFamilyNameKana();
+                this.userLastNameKana = user.getUserLastNameKana();
+                this.userMail = user.getUserMail();
+                this.userGender = user.getUserGender();
+                this.userPostalCode = user.getUserPostalCode();
+                this.userPrefecture = user.getUserPrefecture();
+                this.userAddress1 = user.getUserAddress1();
+                this.userAddress2 = user.getUserAddress2();
+                this.userAuthority = user.getUserAuthority();
+
+            } else {
+
+            	addActionError("該当するユーザーが見つかりません。");
+
+                return ERROR; // ユーザーが見つからなかった場合
+
+            }
+
+        }
 
 		return SUCCESS;
+
 	}
 
 	// ゲッター・セッター
-	public int getUserId() {
-		return userId;
-	}
+    public int getUserId() {
+        return userId;
+    }
 
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
 
 	public String getUserFamilyName() {
-		return userFamilyName;
+        return userFamilyName;
 
-	}
+    }
+    public void setUserFamilyName(String userFamilyName) {
+        this.userFamilyName = userFamilyName;
 
-	public void setUserFamilyName(String userFamilyName) {
-		this.userFamilyName = userFamilyName;
-
-	}
+    }
 
 	public String getUserLastName() {
-		return userLastName;
+        return userLastName;
 
-	}
+    }
 
-	public void setUserLastName(String userLastName) {
-		this.userLastName = userLastName;
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
 
-	}
+    }
 
-	public String getUserFamilyNameKana() {
-		return userFamilyNameKana;
+ 	public String getUserFamilyNameKana() {
+ 		return userFamilyNameKana;
 
-	}
+ 	}
 
-	public void setUserFamilyNameKana(String userFamilyNameKana) {
-		this.userFamilyNameKana = userFamilyNameKana;
+ 	public void setUserFamilyNameKana(String userFamilyNameKana) {
+ 		this.userFamilyNameKana = userFamilyNameKana;
 
-	}
+ 	}
 
-	public String getUserLastNameKana() {
-		return userLastNameKana;
+ 	public String getUserLastNameKana() {
+ 		return userLastNameKana;
 
-	}
+ 	}
 
-	public void setUserLastNameKana(String userLastNameKana) {
-		this.userLastNameKana = userLastNameKana;
+ 	public void setUserLastNameKana(String userLastNameKana) {
+ 		this.userLastNameKana = userLastNameKana;
 
-	}
+ 	}
 
-	public String getUserMail() {
-		return userMail;
+ 	public String getUserMail() {
+ 		return userMail;
 
-	}
+ 	}
 
-	public void setUserMail(String userMail) {
-		this.userMail = userMail;
+ 	public void setUserMail(String userMail) {
+ 		this.userMail = userMail;
 
-	}
+ 	}
 
-	public String getUserPassword() {
-		return userPassword;
+ 	public String getUserPassword() {
+ 		return userPassword;
 
-	}
+ 	}
 
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
+ 	public void setUserPassword(String userPassword) {
+ 		this.userPassword = userPassword;
 
-	}
+ 	}
 
-	public int getUserGender() {
-		return userGender;
+ 	public int getUserGender() {
+ 		return userGender;
 
-	}
+ 	}
 
-	public void setUserGender(int userGender) {
-		System.out.println("start: setUserGender()");
-		System.out.println(userGender);
-		this.userGender = userGender;
-		System.out.println("end: setUserGender()");
+ 	public void setUserGender(int userGender) {
+ 		System.out.println("start: setUserGender()");
+ 		System.out.println(userGender);
+ 		this.userGender = userGender;
+ 		System.out.println("end: setUserGender()");
 
-	}
+ 	}
 
-	public String getUserPostalCode() {
+ 	public String getUserPostalCode() {
 		return userPostalCode;
 
 	}
@@ -148,68 +162,69 @@ public class UpdateAction extends ActionSupport {
 	public void setUserPostalCode(String userPostalCode) {
 		System.out.println("start: setUserPostalCode()");
 		System.out.println(userPostalCode);
-		// if (userPostalCode == null) {
-		// System.out.println("郵便番号が未入力です");
-		//
-		// }
+//		if (userPostalCode == null) {
+//	        System.out.println("郵便番号が未入力です");
+//
+//		}
 		this.userPostalCode = userPostalCode;
 		System.out.println("end: setUserPostalCode()");
 
 	}
+/*
+ 	public String getUserPostalCodeCheck() {
+ 		return userPostalCodeCheck;
 
-	/*
-	 * public String getUserPostalCodeCheck() { return userPostalCodeCheck;
-	 *
-	 * }
-	 *
-	 * public void setUserPostalCodeCheck(String userPostalCodeCheck) {
-	 * System.out.println("start: setUserPostalCodeCheck()");
-	 * System.out.println(userPostalCodeCheck); this.userPostalCodeCheck =
-	 * userPostalCodeCheck; System.out.println("end: setUserPostalCodeCheck()");
-	 *
-	 * }
-	 */
-	public String getUserPrefecture() {
-		return userPrefecture;
+ 	}
 
-	}
+ 	public void setUserPostalCodeCheck(String userPostalCodeCheck) {
+ 		System.out.println("start: setUserPostalCodeCheck()");
+ 		System.out.println(userPostalCodeCheck);
+ 		this.userPostalCodeCheck = userPostalCodeCheck;
+ 		System.out.println("end: setUserPostalCodeCheck()");
 
-	public void setUserPrefecture(String userPrefecture) {
-		this.userPrefecture = userPrefecture;
+ 	}
+*/
+ 	public String getUserPrefecture() {
+ 		return userPrefecture;
 
-	}
+ 	}
 
-	public String getUserAddress1() {
-		return userAddress1;
+ 	public void setUserPrefecture(String userPrefecture) {
+ 		this.userPrefecture = userPrefecture;
 
-	}
+ 	}
 
-	public void setUserAddress1(String userAddress1) {
-		this.userAddress1 = userAddress1;
+ 	public String getUserAddress1() {
+ 		return userAddress1;
 
-	}
+ 	}
 
-	public String getUserAddress2() {
-		return userAddress2;
+ 	public void setUserAddress1(String userAddress1) {
+ 		this.userAddress1 = userAddress1;
 
-	}
+ 	}
 
-	public void setUserAddress2(String userAddress2) {
-		this.userAddress2 = userAddress2;
+ 	public String getUserAddress2() {
+ 		return userAddress2;
 
-	}
+ 	}
 
-	public int getUserAuthority() {
-		return userAuthority;
+ 	public void setUserAddress2(String userAddress2) {
+ 		this.userAddress2 = userAddress2;
 
-	}
+ 	}
 
-	public void setUserAuthority(int userAuthority) {
-		System.out.println("start: setUserAuthority()");
-		System.out.println(userAuthority);
-		this.userAuthority = userAuthority;
-		System.out.println("end: setUserAuthority()");
+ 	public int getUserAuthority() {
+ 		return userAuthority;
 
-	}
+ 	}
+
+ 	public void setUserAuthority(int userAuthority) {
+ 		System.out.println("start: setUserAuthority()");
+ 		System.out.println(userAuthority);
+ 		this.userAuthority = userAuthority;
+ 		System.out.println("end: setUserAuthority()");
+
+ 	}
 
 }
